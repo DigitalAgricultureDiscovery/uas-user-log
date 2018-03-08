04480c65a92bde02b3a345001471c2590e7c519a
Ben Hancock <ben.hancock@gmail.com>
2018-03-08 16:10:00 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import Header from './Header';
+import LogbookForm from './LogbookForm';
+//
+// const mapStateToProps = function(state) {
+//   return {
+//     something: state.something,
+//   }
+// }
+//
+// const mapDispatchToProps = function(dispatch) {
+//   return bindActionCreators({
+//     getSomething: actionCreators.getSomething,
+//   }, dispatch);
+// }
+
+const showResults = values =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      // simulate server latency
+      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
+      resolve();
+    }, 500)
+  });
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <MuiThemeProvider>
+              <Header />
+            </MuiThemeProvider>
+            <LogbookForm onSubmit={ showResults } />
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/pages/Welcome.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { reduxForm } from 'redux-form';
+import { CardActions, CardTitle, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import validate from './utils/validate';
+
+class Welcome extends React.Component {
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit}>
+        <CardTitle title="Welcome" />
+        <CardText>
+          <p>Welcome to Simple Logbook. Choose below if you
+          will be creating a new log or loading a previously saved
+          one.</p>
+        </CardText>
+        <CardActions>
+          <RaisedButton
+            className="next"
+            label="Next"
+            type="submit"
+            primary={true}
+          />
+        </CardActions>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'logbook',
+  destroyOnUnmount: false,
+  forceUnregisterOnUnmount: true,
+  validate
+})(Welcome)
