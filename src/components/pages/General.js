45dbdbc074525817336a90e91054adcc0c4d801c
Ben Hancock <ben.hancock@gmail.com>
2018-03-11 23:58:24 -0400

    revise add flight page on form

--- src/components/pages/General.js
+++ src/components/pages/General.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
+import { Field, FieldArray, reduxForm } from 'redux-form';
 // material-ui elements
 import { DatePicker, TextField, TimePicker } from 'redux-form-material-ui';
 import { CardActions, CardTitle, CardText }  from 'material-ui/Card';
+import Divider                               from 'material-ui/Divider';
 import FlatButton                            from 'material-ui/FlatButton';
+import IconButton                            from 'material-ui/IconButton';
 import RaisedButton                          from 'material-ui/RaisedButton';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+}                                            from 'material-ui/Table';
 // material-ui icons
-import MyLocation from 'material-ui/svg-icons/maps/my-location';
+import ActionFlightTakeoffIcon from 'material-ui/svg-icons/action/flight-takeoff';
+import DeleteForeverIcon       from 'material-ui/svg-icons/action/delete-forever';
+import MyLocationIcon          from 'material-ui/svg-icons/maps/my-location';
+import SaveIcon                from 'material-ui/svg-icons/content/save';
+// material-ui colors
+import {red500, lightGreen500} from 'material-ui/styles/colors';
 
 import validate from './utils/validate';
 import locationUtil from './utils/location';
@@ -18,17 +33,67 @@ class FlightDatePicker extends React.Component {
         name="flightDatePicker"
         component={DatePicker}
         format={null}
-        floatingLabelText="Date of flight"
+        floatingLabelText="Date of flight(s)"
       />
     )
   }
 }
 
+class AddFlightButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.addNewFlight();
+  }
+
+  render() {
+    return (
+      <RaisedButton
+        label="Add Flight"
+        labelPosition="before"
+        backgroundColor={lightGreen500}
+        icon={<ActionFlightTakeoffIcon />}
+        onClick={this.handleClick}
+      />
+    )
+  }
+}
+
+const renderFlights = ({ fields, change }) => (
+  <div>
+    <ul style={{listStyleType: "none", padding: 0}}>
+      {fields.map((flight, index) =>
+        <li key={index}>
+          <strong>Flight #{index + 1}</strong>
+          <IconButton
+            tooltip="Remove flight"
+            onClick={() => fields.remove(index)}
+          >
+            <DeleteForeverIcon color={red500} />
+          </IconButton>
+          <FlightTimeStartPicker fieldName={`${flight}.flightTimeStart`} />
+          <FlightTimeEndPicker fieldName={`${flight}.flightTimeEnd`} />
+          <Location
+            fieldLatName={`${flight}.flightLatLocation`}
+            fieldLonName={`${flight}.flightLonLocation`}
+            change={change}
+          />
+          <br />
+        </li>
+      )}
+    </ul>
+    <AddFlightButton addNewFlight={() => fields.push({})} />
+  </div>
+);
+
 class FlightTimeStartPicker extends React.Component {
   render() {
     return (
       <Field
-        name="flightTimeStartPicker"
+        name={this.props.fieldName}
         component={TimePicker}
         format={null}
         floatingLabelText="Start of flight time"
@@ -41,7 +106,7 @@ class FlightTimeEndPicker extends React.Component {
   render() {
     return (
       <Field
-        name="flightTimeEndPicker"
+        name={this.props.fieldName}
         component={TimePicker}
         format={null}
         floatingLabelText="End of flight time"
@@ -59,40 +124,66 @@ class Location extends React.Component {
   handleClick() {
     locationUtil.clientLocation()
       .then(coords => {
-        this.props.change('latitudeText', coords.latitude);
-        this.props.change('longitudeText', coords.longitude);
+        this.props.change(this.props.fieldLatName, coords.latitude);
+        this.props.change(this.props.fieldLonName, coords.longitude);
       })
       .catch(err => console.log(err));
   }
 
   render() {
-    const styles = {
-      button: {
-        margin: 12,
-      },
-    };
     return (
       <div>
         <Field
-          name="latitudeText"
+          name={this.props.fieldLatName}
           component={TextField}
           floatingLabelText="Latitude"
         />
+        <br />
         <Field
-          name="longitudeText"
+          name={this.props.fieldLonName}
           component={TextField}
           floatingLabelText="Longitude"
         />
+        <br />
         <RaisedButton
           onClick={this.handleClick}
           label="My location"
           labelPosition="before"
-          backgroundColor="#a4c639"
-          icon={<MyLocation />}
-          style={styles.button}
+          icon={<MyLocationIcon />}
         />
       </div>
     )
+  }
+}
+
+class FlightTable extends React.Component {
+  render() {
+    const flightData = [
+      {id: 1, start: '9:00', end: '9:35', location: '45, 82'},
+      {id: 2, start: '1:30', end: '2:15', location: '45, 83'},
+      {id: 3, start: '3:00', end: '3:45', location: '44, 83'},
+    ];
+    return (
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderColumn>ID</TableHeaderColumn>
+            <TableHeaderColumn>Start time</TableHeaderColumn>
+            <TableHeaderColumn>End time</TableHeaderColumn>
+            <TableHeaderColumn>Location</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {flightData.map((row, i) =>
+            <TableRow key={row.id}>
+              <TableRowColumn>{row.start}</TableRowColumn>
+              <TableRowColumn>{row.end}</TableRowColumn>
+              <TableRowColumn>{row.location}</TableRowColumn>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    );
   }
 }
 
@@ -105,9 +196,7 @@ class General extends React.Component {
         <CardTitle title="General" />
         <CardText>
           <FlightDatePicker />
-          <FlightTimeStartPicker />
-          <FlightTimeEndPicker />
-          <Location change={this.props.change}/>
+          <FieldArray name="flights" component={renderFlights} change={this.props.change} />
         </CardText>
         <CardActions>
           <FlatButton
@@ -126,7 +215,6 @@ class General extends React.Component {
     )
   }
 }
-
 export default reduxForm({
   form: 'logbook',
   destroyOnUnmount: false,
