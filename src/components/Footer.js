1b2c39aa19cb1605a0ad036d2a0e12e61bf5d562
Ben Hancock <hancocb@purdue.edu>
2018-03-13 12:25:21 -0400

    footer revisions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
 
 class Footer extends React.Component {
   render() {
     return (
-      <div style={{backgroundColor: "#000000", marginTop: 25}}>
-        <a href="https://purdue.edu" target="_blank" rel="noopener noreferrer">
-          <img
-            src="./images/logos/Purdue-Sig-Black-Gold-rgb.png"
-            alt="Purdue logo"
-            style={{height: 45}} />
-        </a>
-        <a href="https://saaesd.org/" target="_blank" rel="noopener noreferrer">
-          <img
-            src="./images/logos/logo_logbook.jpeg"
-            alt="SAAESD logo"
-            style={{height: 45}} />
-        </a>
-      </div>
+      <footer>
+        <div style={{backgroundColor: "#BAA892", marginTop: 25, padding: 25, textAlign: "center"}}>
+          <span style={{marginRight: "80%"}}>
+            <a href="https://purdue.edu" target="_blank" rel="noopener noreferrer">
+              <img
+                src="./images/logos/Purdue-Sig-White-rgb.png"
+                alt="Purdue logo"
+                style={{height: 45}} />
+            </a>
+          </span>
+          <span>
+            <a href="https://saaesd.org/" target="_blank" rel="noopener noreferrer">
+              <img
+                src="./images/logos/logo_logbook.jpeg"
+                alt="SAAESD logo"
+                style={{height: 45}} />
+            </a>
+          </span>
+        </div>
+      </footer>
     )
   }
 }
