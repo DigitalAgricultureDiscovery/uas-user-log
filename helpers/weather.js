ad2875e045bb8c71f0c914de0aa151c4d0e91950
Ben Hancock <hancocb@purdue.edu>
2018-03-14 14:34:48 -0400

    fetch apixu forecast on server and return to client

---
+++ helpers/weather.js
@@ -0,0 +1,9 @@
+const fetch = require('node-fetch');
+
+// fetch 7 day forecast for location using apixu
+exports.getForecast = async (key, location) => {
+  const apiURL = 'http://api.apixu.com/v1/forecast.json?key=' + key + '&q=' + location + '&days=7';
+  const response = await fetch(apiURL);
+  const data = await response.json();
+  return data;
+}
