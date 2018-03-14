870d7fe079e354674fa82c321805c6be3ef429a1
Ben Hancock <ben.hancock@gmail.com>
2018-03-14 10:34:19 -0400

    add sticky footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,33 @@
 import React from 'react';
+import { Table, TableBody, TableRow, TableRowColumn } from 'material-ui/Table';
 
 class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <div style={{backgroundColor: "#BAA892", marginTop: 25, padding: 25, textAlign: "center"}}>
-          <span style={{marginRight: "80%"}}>
-            <a href="https://purdue.edu" target="_blank" rel="noopener noreferrer">
-              <img
-                src="./images/logos/Purdue-Sig-White-rgb.png"
-                alt="Purdue logo"
-                style={{height: 45}} />
-            </a>
-          </span>
-          <span>
-            <a href="https://saaesd.org/" target="_blank" rel="noopener noreferrer">
-              <img
-                src="./images/logos/logo_logbook.jpeg"
-                alt="SAAESD logo"
-                style={{height: 45}} />
-            </a>
-          </span>
+        <div className="footer">
+          <Table style={{backgroundColor: "inherit"}}>
+            <TableBody displayRowCheckbox={false}>
+              <TableRow>
+                <TableRowColumn style={{textAlign: "left"}}>
+                  <a href="https://purdue.edu" target="_blank" rel="noopener noreferrer">
+                    <img
+                      src="./images/logos/Purdue-Sig-White-rgb.png"
+                      alt="Purdue logo"
+                      style={{width: "auto", maxHeight: 50}} />
+                  </a>
+                </TableRowColumn>
+                <TableRowColumn style={{textAlign: "right"}}>
+                  <a href="https://saaesd.org/" target="_blank" rel="noopener noreferrer">
+                    <img
+                      src="./images/logos/logo_transparent.png"
+                      alt="SAAESD logo"
+                      style={{width: "auto", maxHeight: 50}} />
+                  </a>
+                </TableRowColumn>
+              </TableRow>
+            </TableBody>
+          </Table>
         </div>
       </footer>
     )
