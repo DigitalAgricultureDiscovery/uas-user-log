30bb55d0c105d24fbec3890ee774e7bd9ba4c970
Ben Hancock <hancocb@purdue.edu>
2018-03-19 17:45:00 -0400

    four unique tables for four sensors on processed page

--- client/src/components/pages/Processed.js
+++ client/src/components/pages/Processed.js
@@ -19,9 +19,34 @@ import {
 
 import validate from '../helpers/validate';
 
-const wordWrapStyle = {
-  wordWrap: 'break-word',
-  whiteSpace: 'normal',
+const SENSORS = [
+  {value: 1, name: 'Visible'},
+  {value: 2, name: 'Radar'},
+  {value: 3, name: 'Infrared'},
+  {value: 4, name: 'Multispectral'},
+]
+
+class NiirsSensorSelect extends React.Component {
+  menuItems(sensors) {
+    return sensors.map((sensor) => (
+      <MenuItem
+        key={sensor.value}
+        value={sensor.value}
+        primaryText={sensor.name}
+      />
+    ))
+  }
+  render() {
+    return (
+      <Field
+        name="niirsSensorSelect"
+        component={SelectField}
+        floatingLabelText="Select a sensor"
+      >
+        {this.menuItems(SENSORS)}
+      </Field>
+    )
+  }
 }
 
 class NiirsGrid extends React.Component {
@@ -32,16 +57,16 @@ class NiirsGrid extends React.Component {
         <div style={{gridColumn: 2, gridRow: 1, borderBottom: 'solid'}}><strong>Civilian NIIRS rating</strong></div>
         <div style={{gridColumn: '3/5', gridRow: 1, borderBottom: 'solid'}}><strong>Features that can be distinguished</strong></div>
         {/* checkboxes */}
-        <div style={{gridColumn: 1, gridRow: 2}}><Field name="rating0" component={Checkbox} /></div>
-        <div style={{gridColumn: 1, gridRow: 3}}><Field name="rating1" component={Checkbox} /></div>
-        <div style={{gridColumn: 1, gridRow: 4}}><Field name="rating2" component={Checkbox} /></div>
-        <div style={{gridColumn: 1, gridRow: 5}}><Field name="rating3" component={Checkbox} /></div>
-        <div style={{gridColumn: 1, gridRow: 6}}><Field name="rating4" component={Checkbox} /></div>
-        <div style={{gridColumn: 1, gridRow: 7}}><Field name="rating5" component={Checkbox} /></div>
-        <div style={{gridColumn: 1, gridRow: 8}}><Field name="rating6" component={Checkbox} /></div>
-        <div style={{gridColumn: 1, gridRow: 9}}><Field name="rating7" component={Checkbox} /></div>
-        <div style={{gridColumn: 1, gridRow: 10}}><Field name="rating8" component={Checkbox} /></div>
-        <div style={{gridColumn: 1, gridRow: 11}}><Field name="rating9" component={Checkbox} /></div>
+        <div style={{gridColumn: 1, gridRow: 2}}><Field name={`${this.props.fieldName}.rating0`} component={Checkbox} /></div>
+        <div style={{gridColumn: 1, gridRow: 3}}><Field name={`${this.props.fieldName}.rating1`} component={Checkbox} /></div>
+        <div style={{gridColumn: 1, gridRow: 4}}><Field name={`${this.props.fieldName}.rating2`} component={Checkbox} /></div>
+        <div style={{gridColumn: 1, gridRow: 5}}><Field name={`${this.props.fieldName}.rating3`} component={Checkbox} /></div>
+        <div style={{gridColumn: 1, gridRow: 6}}><Field name={`${this.props.fieldName}.rating4`} component={Checkbox} /></div>
+        <div style={{gridColumn: 1, gridRow: 7}}><Field name={`${this.props.fieldName}.rating5`} component={Checkbox} /></div>
+        <div style={{gridColumn: 1, gridRow: 8}}><Field name={`${this.props.fieldName}.rating6`} component={Checkbox} /></div>
+        <div style={{gridColumn: 1, gridRow: 9}}><Field name={`${this.props.fieldName}.rating7`} component={Checkbox} /></div>
+        <div style={{gridColumn: 1, gridRow: 10}}><Field name={`${this.props.fieldName}.rating8`} component={Checkbox} /></div>
+        <div style={{gridColumn: 1, gridRow: 11}}><Field name={`${this.props.fieldName}.rating9`} component={Checkbox} /></div>
         {/* rating level */}
         <div style={{gridColumn: 2, gridRow: 2}}>Rating Level 0</div>
         <div style={{gridColumn: 2, gridRow: 3}}>Rating Level 1<br />{"(< 9 meters)"}</div>
@@ -70,14 +95,55 @@ class NiirsGrid extends React.Component {
 }
 
 class Processed extends React.Component {
+  renderSensorGrid(sensorIndex) {
+    switch(sensorIndex) {
+      case 1:
+        return (
+          <div>
+            <Subheader>Visible NIIRS - Select one or more applicable ratings</Subheader>
+            <NiirsGrid fieldName={"vis"} />
+          </div>
+        );
+      case 2:
+        return (
+          <div>
+            <Subheader>Radar NIIRS - Select one or more applicable ratings</Subheader>
+            <NiirsGrid fieldName={"rad"} />
+          </div>
+        );
+      case 3:
+        return (
+          <div>
+            <Subheader>Infrared NIIRS - Select one or more applicable ratings</Subheader>
+            <NiirsGrid fieldName={"inf"} />
+          </div>
+        );
+      case 4:
+        return (
+          <div>
+            <Subheader>Multispectral NIIRS - Select one or more applicable ratings</Subheader>
+            <NiirsGrid fieldName={"mul"} />
+          </div>
+        );
+      default:
+        return <div>Unknown sensor selected</div>;
+    }
+  }
+
   render() {
-    const { handleSubmit, previousPage } = this.props;
+    const { handleSubmit, previousPage, currentNiirsSensor } = this.props;
     return (
       <form onSubmit={handleSubmit}>
         <CardTitle title="Processed" />
         <CardText>
-          <Subheader>Remote Sensing - Select one or more applicable ratings</Subheader>
-          <NiirsGrid />
+          <NiirsSensorSelect />
+          <br /><br />
+          {this.renderSensorGrid(currentNiirsSensor)}
+          <br /><br />
+          <strong>Source: </strong>
+          <a href="https://fas.org/irp/imint/niirs.htm" target="_blank" rel="noopener noreferrer">
+            National Image Interpretability Rating Scales
+          </a>
         </CardText>
         <CardActions>
           <FlatButton
@@ -98,9 +164,20 @@ class Processed extends React.Component {
   }
 }
 
-export default reduxForm({
+const myReduxForm = reduxForm({
   form: 'logbook',
   destroyOnUnmount: false,
   forceUnregisterOnUnmount: true,
   validate,
 })(Processed);
+
+const selector = formValueSelector('logbook');
+export default connect(
+  state => {
+    const currentNiirsSensor = selector(state, 'niirsSensorSelect');
+
+    return {
+      currentNiirsSensor,
+    }
+  }
+)(myReduxForm);
