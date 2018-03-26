285c60bd77dd5ef95f200dd6f097f6327d0ac606
Ben Hancock <hancocb@purdue.edu>
2018-03-26 11:21:15 -0400

    add file with default initial values

---
+++ client/src/components/helpers/DefaultInitialValues.js
@@ -0,0 +1,35 @@
+const initialValues = {
+  'mission_Type': 2,
+
+  'crop_LifeCycle': 1,
+
+  'hardware_Type': 1,
+  'hardware_RemoteChargeTarget': 100.00,
+  'hardware_RemoteChargeMinimum': 30.00,
+  'hardware_GroundControlChargeTarget': 100.00,
+  'hardware_GroundControlChargeMinimum': 30.00,
+
+  'flightOperation_Mode': 1,
+
+  'dataCollection_ChemicalType': 1,
+  'dataCollection_ApplicationRateUnit': 1,
+  'dataCollection_PressureUnit': 1,
+  'dataCollection_SwathDistanceUnit': 1,
+  'dataCollection_SwathAreaUnit': 1,
+  'dataCollection_ApplicationType': 1,
+
+  'b4ufly_Status': 1,
+  'b4ufly_Preflight': 'yes',
+  'b4ufly_Permission': 'notRequired',
+
+  'flightParameters_AGLMaximum': 400,
+  'flightParameters_AGLUnit': 1,
+  'flightParameters_LookAngle': 'vertical',
+  'flightParameters_MaximumGroundSpeed': 'no',
+
+  'people_PeoplePresent': 'no',
+
+  'processed_NIIRS': 1,
+}
+
+module.exports = initialValues;
