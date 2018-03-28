c1e8a10ac468a11675a59936cf0b8c2055be2a04
Ben Hancock <hancocb@purdue.edu>
2018-03-27 23:32:13 -0400

    implement filesaver

--- client/src/components/pages/Finish.js
+++ client/src/components/pages/Finish.js
@@ -6,52 +6,64 @@ import { CardActions, CardTitle, CardText } from 'material-ui/Card';
 import FlatButton                           from 'material-ui/FlatButton';
 import RaisedButton                         from 'material-ui/RaisedButton';
 
+import { saveAs } from 'file-saver';
+
 import validate from '../helpers/validate';
 
-const Finish = props => {
-  const { handleSubmit, pristine, previousPage, submitting, formValues, clearAndReturn } = props;
-  let data = "data:text/json;charset=utf-8,";
-  data += encodeURIComponent(JSON.stringify(formValues));
-  return (
-    <form onSubmit={ handleSubmit }>
-      <CardTitle title="Finish" />
-      <CardText>
-        <p>
-          Click Save Copy to download a copy of the form you have just completed.
-          You will be able to re-load your form selections using the downloaded file
-          on the Welcome page. Editing this file may prevent the site from properly
-          loading the file. Your forms are never saved or read on a remote server.
-        </p>
-        <p>
-          Click the Clear button to erase your current form selections and return
-          to the beginning of the form.
-        </p>
-      </CardText>
-      <CardActions>
-        <FlatButton
-          className="previous"
-          label="Previous"
-          onClick={previousPage}
-          backgroundColor="#BAA892"
-        />
-        <RaisedButton
-          className="saveCopy"
-          label="Save Copy"
-          href={data}
-          download="data.json"
-          backgroundColor="#FF9B1A"
-          disabled={pristine || submitting}
-        />
-        <RaisedButton
-          className="clearReturn"
-          label="Clear Form"
-          labelPosition="before"
-          onClick={clearAndReturn}
-          backgroundColor="#AD1F65"
-        />
-      </CardActions>
-    </form>
-  )
+class Finish extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const blob = new Blob([JSON.stringify(this.props.formValues)], {type: 'text/json;charset=utf-8'});
+    saveAs(blob, 'data.json');
+  }
+
+  render() {
+    const { handleSubmit, pristine, previousPage, submitting, clearAndReturn } = this.props;
+
+    return (
+      <form onSubmit={ handleSubmit }>
+        <CardTitle title="Finish" />
+        <CardText>
+          <p>
+            Click Save Copy to download a copy of the form you have just completed.
+            You will be able to re-load your form selections using the downloaded file
+            on the Welcome page. Editing this file may prevent the site from properly
+            loading the file. Your forms are never saved or read on a remote server.
+          </p>
+          <p>
+            Click the Clear button to erase your current form selections and return
+            to the beginning of the form.
+          </p>
+        </CardText>
+        <CardActions>
+          <FlatButton
+            className="previous"
+            label="Previous"
+            onClick={previousPage}
+            backgroundColor="#BAA892"
+          />
+          <RaisedButton
+            className="saveCopy"
+            label="Save Copy"
+            backgroundColor="#FF9B1A"
+            disabled={pristine || submitting}
+            onClick={this.handleClick}
+          />
+          <RaisedButton
+            className="clearReturn"
+            label="Clear Form"
+            labelPosition="before"
+            onClick={clearAndReturn}
+            backgroundColor="#AD1F65"
+          />
+        </CardActions>
+      </form>
+    )
+  }
 }
 
 const myReduxForm = reduxForm({
