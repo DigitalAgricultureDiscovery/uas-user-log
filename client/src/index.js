a4381ba4292786fc982ce6576b37d274d29a6550
Ben Hancock <hancocb@purdue.edu>
2018-03-29 09:25:40 -0400

    remove now ignored node_modules, add disabled google analytics

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux'
 import { reducer as reduxFormReducer } from 'redux-form'
 
 import App from './components/App';
+
+// ReactGA.initialize('UA-78284792-5');
+// ReactGA.pageview(window.location.pathname + window.location.search);
 
 const reducer = combineReducers({
   form: reduxFormReducer,
