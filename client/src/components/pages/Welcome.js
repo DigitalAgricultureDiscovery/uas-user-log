21496a72011aa608264b61909e9d15f7759cc2bb
Ben Hancock <hancocb@purdue.edu>
2018-04-04 10:01:50 -0400

    update welcome message

--- client/src/components/pages/Welcome.js
+++ client/src/components/pages/Welcome.js
@@ -115,12 +115,12 @@ class Welcome extends React.Component {
           <p>
             The UAS User Log is a server-based, digital logbook that is
             accessible through any web browser on internet-connected devices.​
-            It is an outcome of multistate teams working together to develop a
-            common protocol for unmanned aircraft systems (UAS) operation for
-            activities such as research/production and spray
-            application. It ​relies on simple user interactions to develop a
-            record of UAS mission and can also serve to enhance your flight
-            and maintenance experience.​
+            It is an outcome of multi-state teams working together to develop
+            a common protocol for unmanned aircraft systems (UAS) operation
+            for purposes such as research/production, spray application, and
+            any other activity of interest . It ​relies on simple user
+            interactions to develop a record of UAS mission and can also serve
+            to enhance your flight and maintenance experience.​
           </p>
           <Divider />
           <p>
