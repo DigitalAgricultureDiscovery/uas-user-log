e35ed031c31e0ea4ddb712d58c7bd716f2f90cfd
Ben Hancock <hancocb@purdue.edu>
2018-04-05 12:44:24 -0400

    add button for user feedback

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import EmailIcon from 'material-ui/svg-icons/communication/email';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Header extends React.Component {
   render() {
@@ -7,6 +9,17 @@ class Header extends React.Component {
       <AppBar
         title="UAS User Log"
         iconElementLeft={(<div/>)}
+        iconElementRight={(
+          <address>
+            <RaisedButton
+              href="mailto:uasuserlog@gmail.com?Subject=User%20feedback"
+              target="_top"
+              label="Feedback"
+              style={{margin: 5}}
+              icon={<EmailIcon />}
+            />
+          </address>
+        )}
         style={{backgroundColor: "#000000"}}
       />
     )
