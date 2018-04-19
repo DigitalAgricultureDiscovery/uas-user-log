6881c1c3227a9d577d2c252a36c2087608ee3ce8
Ben Hancock <hancocb@purdue.edu>
2018-04-19 12:01:28 -0400

    fix to manifest start_url

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -67,12 +67,14 @@ function registerValidSW(swUrl, updateServiceWorkerStatus) {
               // It's the perfect time to display a "New content is
               // available; please refresh." message in your web app.
               console.log('New content is available; please refresh.');
+              // Notify user the sw has been updated
               updateServiceWorkerStatus(ServiceWorkerStatuses.UPDATED);
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
               console.log('Content is cached for offline use.');
+              // Notify user the sw is registered
               updateServiceWorkerStatus(ServiceWorkerStatuses.REGISTERED);
             }
           }
