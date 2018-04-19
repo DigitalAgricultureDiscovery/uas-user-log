00ddd73109c2df574d7d5b9e3b7ad6d39eaaddd1
Ben Hancock <hancocb@purdue.edu>
2018-04-19 16:27:26 -0400

    add webfontloader

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import WebFont from 'webfontloader';
 import { setServiceWorkerStatus } from './actions';
 import rootReducer from './reducers';
 import App from './components/App';
@@ -15,6 +16,13 @@ const store = (window.devToolsExtension
 const updateServiceWorkerStatus = (status) => {
   store.dispatch(setServiceWorkerStatus(status));
 }
+
+// Load Roboto font
+WebFont.load({
+  google: {
+    families: ['Roboto:300,400,500', 'sans-serif']
+  }
+});
 
 ReactDOM.render(
   <Provider store={ store }>
